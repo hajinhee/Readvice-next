fce0b32b7b0748a54da566ef3c4b5765614ab342
HAJINHEE <hajinheee@gmail.com>
2022-05-09 15:07:12 +0900

    first commit

---
+++ modules/index.js
@@ -0,0 +1,28 @@
+import {combineReducers} from 'redux';
+import {all} from 'redux-saga/effects';
+import counter, {counterSaga} from './common/counter';
+import register, {registerSaga} from './auth/register';
+import login, {loginSaga} from './auth/login';
+import {HYDRATE} from "next-redux-wrapper"
+const rootReducer = combineReducers({
+    index: (state = {}, action) => {
+        switch (action.type) {
+            case HYDRATE:
+                console.log("HYDRATE", action);
+                return {
+                    ...state,
+                    ...action.payload
+                };
+            default:
+                return state;
+        }
+    },
+    counter,
+    login,
+    register
+});
+export function* rootSaga() {
+    yield all([counterSaga(), registerSaga(), loginSaga()]);
+}
+
+export default rootReducer;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import {Layout, Home} from '@/components'
+import React, {useEffect} from 'react';
+import axios from "axios";
+import styles from '../styles/Home.module.css'
+
+export default function HomePage() {
+    return (<Layout>
+        <Home/>
+    </Layout>)
+}
